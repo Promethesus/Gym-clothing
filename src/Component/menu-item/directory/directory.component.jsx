dea95835270debc7f496ad6a39c16777a27ff554
Promethesus <jeremyandrews707@gmail.com>
2021-10-12 19:00:29 -0700

    updated react web app to contain basic styling and state and componenets. Also starting document for describing flow of data with visual diagram

---
+++ src/Component/menu-item/directory/directory.component.jsx
@@ -0,0 +1,58 @@
+import MenuItem from './Menu-Item/menu-item.component'
+import './directory.styles.scss'
+import React from "react"
+
+class Directory extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            sections: [{
+                title: 'BRO TANKS',
+                imageUrl: 'https://i.ibb.co/X8mnfvp/BRO-TANK.png',
+                id: 1,
+                linkUrl: 'shop/brotanks'
+              },
+              {
+                title: 'JACKETS',
+                imageUrl: 'https://i.ibb.co/KWsxHr3/JACKET.png',
+                id: 2,
+                linkUrl: 'shop/jackets'
+              },
+              {
+                title: 'SWEATS',
+                imageUrl: 'https://i.ibb.co/fpwLNQY/SWEATS.jpg',
+                id: 3,
+                linkUrl: 'shop/sweats'
+              },
+              {
+                title: 'WOMENS',
+                imageUrl: 'https://i.ibb.co/sbKMDGL/WOEMNS.jpg',
+                size: 'large',
+                id: 4,
+                linkUrl: 'shop/womens'
+              },
+              {
+                title: 'MENS',
+                imageUrl: 'https://i.ibb.co/GFyj5bS/MENS.jpg',
+                size: 'large',
+                id: 5,
+                linkUrl: 'shop/mens'
+                }
+            ]
+        }
+    }
+    render() {
+        return (
+            <div className="directory-menu">
+                {
+                    this.state.sections.map(({title, imageUrl, id, size})=>(
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size = {size} />
+                    ))
+                }
+            </div>
+        );
+    }
+}
+
+export default Directory;
