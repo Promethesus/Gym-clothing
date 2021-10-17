9f0ca77804f1d2ce5a076926a5510e141267a0fd
Promethesus <jeremyandrews707@gmail.com>
2021-10-17 05:29:52 -0700

    added higher order component to menu-item for basic routing. Used withRouter for dynamically routing to different pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 import HomePage from './Pages/homepage/homepage.component.jsx'
+import { Switch, Route } from 'react-router-dom'
+
+const BroTankPage = () => (
+  <div>
+    <h1>
+      brotanks
+    </h1>
+  </div>
+)
+
+const JacketsPage = () => (
+  <div>
+    <h1>
+      jackets
+    </h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='shop/brotanks' component={BroTankPage}/>
+        <Route path='/jackets' component={JacketsPage}/>
+      </Switch>
     </div>
   );
 }
