9885e10f3f50fa8bec287809547b32f8cf089c05
Promethesus <jeremyandrews707@gmail.com>
2021-10-20 07:39:24 -0700

    added header page with routing to basic pages.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import HomePage from './Pages/homepage/homepage.component.jsx'
 import ShopPage from './Pages/shop/shop.component.jsx';
 import { Switch, Route } from 'react-router-dom'
-
+import Header from './Component/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
