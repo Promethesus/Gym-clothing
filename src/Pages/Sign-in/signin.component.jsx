fe17f2ca411a6db2555b0f5a8f5222d48fddddec
Promethesus <jeremyandrews707@gmail.com>
2021-11-16 07:51:32 -0800

    added working sign in and sign up component with routing and firebase

--- src/Pages/Sign-in/signin.component.jsx
+++ src/Pages/Sign-in/signin.component.jsx
@@ -4,7 +4,7 @@ import "./sign-in-styles.scss"
 import FormInput from "../../Component/form-input/form-input.component.jsx";
 import CustomButton from "../../Component/custom-button/custom-button.component";
 
- import {signInWithGoogle} from "../../firebase/firebase.utils"
+ import {auth, signInWithGoogle} from "../../firebase/firebase.utils"
 
 
 class SignInComponent extends React.Component{
@@ -16,9 +16,19 @@ class SignInComponent extends React.Component{
             password:""
         }
     }
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
+        const {email, password} = this.state
+
+        try{
+            await auth.signInWithEmailAndPassword(email,password)
+            this.setState({email: "", password:""})
+        }catch(error){
+            console.error(error)
+        }
+
         this.setState({email: "", password:""})
+
     }
 
     handleChange = event =>{
