28d973fdd0aaaff7b983f9e7bbd46da6a54cf011
Promethesus <jeremyandrews707@gmail.com>
2021-11-16 11:48:04 -0800

    added redux and started switching app from state to redux

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
+import {Provider} from 'react-redux'
+import Store from "../src/Redux/Store"
 
 ReactDOM.render(
+  <Provider store={Store}>
   <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
