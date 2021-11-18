fbb19b626bf15f75a0a359431bc0b81bc471b3ea
Promethesus <jeremyandrews707@gmail.com>
2021-11-18 05:23:54 -0800

    added reducer to header componet and redux to app js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {connect} from 'react-redux'
 import HomePage from './Pages/homepage/homepage.component.jsx'
 import ShopPage from './Pages/shop/shop.component.jsx';
 import { Switch, Route } from 'react-router-dom'
@@ -7,18 +8,11 @@ import SignInPage from './Pages/Sign-in/signin.component';
 import { auth, createUserProfileDocument} from './firebase/firebase.utils'
 import React from 'react';
 import SignUp from './Component/Sign-up-Component/sign-up.component';
+import {setCurrentUser} from './Redux/user/user.actions'
 
 class App extends React.Component {
-  constructor() {
-    super();
-    
-
-    this.state ={
-      currentUser:null
-    }
-  }
-
   unsubscribeFromAuth = null
+
 
   componentDidMount(){
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth =>{
@@ -26,14 +20,12 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth)
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          this.props.setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
           })
         })
-      }else this.setState({currentUser: userAuth});
+      }else setCurrentUser({userAuth});
     })
   }
 
@@ -57,4 +49,8 @@ class App extends React.Component {
   );
 }
 }
-export default App;
+
+const mapDispatchToProp = dispatch => ({
+  setCurrentUser: user=> dispatch(setCurrentUser(user))
+})
+export default connect(null, mapDispatchToProp) (App);
